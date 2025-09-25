2b0d922ae0589acaea961a1133646d70cc780801
Vcz-Jz <kimbor357@hotmail.com>
2025-09-25 17:59:01 -0400

    #1

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+
+//Logo
+import Logo from '../assets/img/Logo.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={Logo} style={styles.logo} />
+        <Text style={styles.title}>WANDER <Text style={ {color: '#DA828F'}}>PETS</Text></Text>
+      </View>
+
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        height: 250,
+        backgroundColor: '#7DFCD5',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 45,
+        color: '#FFA850'
+
+    },
+    logo:{
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+    }
+})
